bd78a891720b8ce6d2fc8263571b68c85bb11fec
odedbarz <odedbarz@gmail.com>
2021-04-15 19:35:39 -0400

    Update pymodel.py

--- Python/pymodel.py
+++ Python/pymodel.py
@@ -2,8 +2,11 @@
 %reload_ext autoreload
 %autoreload 2
 
+from aux import whitening
 import matplotlib.pyplot as plt
 import matplotlib
+# from numpy.core import tests
+# from sklearn.utils import shuffle
 matplotlib.style.use('ggplot')
 
 import os
@@ -16,13 +19,15 @@ import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
 
+from sklearn.model_selection import train_test_split
+
 from mydataset import dataset
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'device: {device}')
 
 
-# %% Load MAT file
+# %% Load from MAT file
 fn = 'data_MUA-ONLY_(08-Jan-2021)_bw(5)_fbands(30)_spec(gammatone).mat'
 path = './'
 Hdry = dataset( drr_idx = 0, unit_number = 1, fn = fn, path = path)
@@ -36,50 +41,30 @@ Hdrr.v = (Hdrr.v - mu)/std
 
 print(Hdrr)
 
+# Pack sequences and labels (next-sequence) into one list
+seq_len = 20   # binwidth * seq_len => duration in msec
+data_length = int(Hdry.n_time-seq_len)
+in_seq = torch.zeros(data_length, seq_len)      # in sequence
+labels = torch.zeros(data_length)               # out sequence
+labels_drr = torch.zeros(data_length)               # out sequence
+for ii in range(data_length):
+    in_seq[ii,:] = Hdrr[ii:ii+seq_len]
+    labels[ii] = Hdry[ii+seq_len]
+    labels_drr[ii] = Hdrr[ii+seq_len]
+ 
+# Split the data into train/test sets
+test_size = 0.2
+X_train, X_test, y_train, y_test = train_test_split(in_seq, labels, test_size=test_size, random_state=42)
+_, _, _, y_test0 = train_test_split(in_seq, labels_drr, test_size=test_size, random_state=42)
 
-# %%
-# train_window = 12   # binwidth * train_window => duration in msec
-# inout_seq = []      # in/out sequence
-# for ii in range(Hdry.n_time-train_window):
-#     seq_ii = Hdrr[ii:ii+train_window]
-#     label_ii = Hdry[ii+train_window]
-#     inout_seq.append((seq_ii, label_ii))
+trainset = TensorDataset( X_train, y_train )
+testset = TensorDataset( X_test, y_test )
 
-# test_size = int(0.2*len(inout_seq))
-# train_seq = inout_seq[:-test_size]
-# test_seq = inout_seq[-test_size:]       # ! USE JACK-KNIFE !
+batch_size = 64
+shuffle = False
+train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle)
+test_loader = DataLoader(testset, batch_size=batch_size, shuffle=shuffle)
 
-train_window = 12   # binwidth * train_window => duration in msec
-in_seq = []      # in/out sequence
-out_seq = []      # in/out sequence
-for ii in range(Hdry.n_time-train_window):
-    seq_ii = Hdrr[ii:ii+train_window]
-    label_ii = Hdry[ii+train_window]
-    in_seq.append(seq_ii.numpy())
-    out_seq.append(label_ii.numpy())
-
-test_size = int(0.2*len(in_seq))
-
-trainset = TensorDataset( in_seq[:-test_size], out_seq[:-test_size] )
-# testset = TensorDataset( inout_seq[-test_size:] )
-
-# # %% Pack data into DATASETs
-# test_size = int(0.2 * len(Hdrr))
-
-# trainset = TensorDataset(Hdrr[:-test_size], Hdry[:-test_size])
-# testset = TensorDataset(Hdrr[-test_size:], Hdry[-test_size:])
-
-# plt.plot(torch.stack(testset[:],1))
-# plt.title('testset')
-# plt.xlabel('Samples')
-# plt.ylabel('Amp')
-
-# # Create DATALOADERs
-# batch_size = 64
-# shuffle = False
-
-# train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle)
-# test_loader  = DataLoader(testset, batch_size=batch_size, shuffle=shuffle)
 
 
 # %% Create RNN model
@@ -104,38 +89,41 @@ class RNN(nn.Module):
         self.h0 = torch.randn(self.n_layers, x.shape[0], self.n_hidden)
 
         # (batch, seq_len, input_size); batch_first == True
-        x = x.view(len(x), 1, -1)       #! batch_size == len(x) == train_window
+        x = x.view(x.shape[0], x.shape[1], 1)       #! batch_size == len(x) == train_window
 
         x, hn = self.rnn(x, self.h0)
         x = self.fc( x[:,-1,:] )          
-        return x[-1]
+        return x #[-1]
 
 
-# Create a model
+# %% Create a model
 n_input = 1
 n_hidden = 100
 n_layers = 1
 n_output = 1
-nonlinearity = 'relu'
+nonlinearity = 'relu' # {'relu', 'tanh'}
 batch_first = True
 dropout = 0
 model = RNN(n_input = n_input, 
             n_hidden = n_hidden, 
             n_layers = n_layers, 
             n_output = n_output,
-            nonlinearity = 'relu', 
+            nonlinearity = nonlinearity, 
             batch_first = True, 
             dropout = 0).to(device)
 
+
 print(model)
-# model(train_seq[0][0])  #! *TEST*
+# X, y = next(iter(train_loader))
+# yhat = model(X)  #! *TEST*
+# plt.plot(np.c_[y.numpy(), yhat.detach().numpy().squeeze()])
 
 
 
 # %% 
 # *** Training ***
 epochs = 100
-lr = 1e-3       # learning rate
+lr = 1e-4       # learning rate
 
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -144,13 +132,14 @@ model.train()
 loss = []
 
 for ep in range(epochs):
-    for k, (seq, y) in enumerate(train_seq):
+    for k, (seq, y) in enumerate(train_loader):
         optimizer.zero_grad()
 
         #! Init the hidden states!!
          
         y_pred = model( seq.to(device) )
-        loss_k = loss_function(y_pred, y[None])
+        y_pred.squeeze_()
+        loss_k = loss_function(y_pred, y)
         loss_k.backward()
         optimizer.step()
 
@@ -166,23 +155,43 @@ print('- Finished training!')
 # %% 
 # *** Testing ***
 model.eval()
-y_pred = []
-y_test = []
-for k, (seq, yk) in enumerate(test_seq):
+y_est = []
+y_dry = []
+for k, (seq, yk) in enumerate(test_loader):
     with torch.no_grad():   # ? DO I NEED no_grad() if I have eval() ? 
         yk_pred = model( seq.to(device) )
-        y_pred.append(yk_pred)
-        y_test.append(yk)
+        y_est.append(yk_pred.numpy())
+        y_dry.append(yk.numpy())
 
-y_pred = np.array(y_pred)
-y_test = np.array(y_test)
-plt.plot(np.c_[y_pred, y_test, Hdrr[-test_size:]])
+# y_pred = np.array(y_pred)
+# y_test = np.array(y_test)
 
-CCdry = np.corrcoef(y_pred, y_test)
-CCdrr = np.corrcoef(y_pred, Hdrr[-test_size:])
-print(f'CCdry: {CCdry[0,1]:8.3f}')
-print(f'CCdrr: {CCdrr[0,1]:8.3f}')
+idx = -1
+y_est = whitening(y_est[idx])
+y_dry = whitening(y_dry[idx])
+# y_drr = whitening(labels_drr[-len(y_est):])
+y_drr = whitening(y_test0[-len(y_est):])
+
+CCdry = np.corrcoef(y_est, y_dry)[0,1]
+CCdrr = np.corrcoef(y_est, y_drr)[0,1]
+
+print(f'CCdry: {CCdry:8.3f}')
+print(f'CCdrr: {CCdrr:8.3f}')
+
+plt.plot(np.c_[y_est, y_dry, y_drr])
+plt.plot(y_dry, label='$y_{dry}$')
+plt.plot(y_est, label='$\hat{y}$')
+plt.plot(y_drr, label='$y_{drr}$')
+plt.legend()
+plt.xlabel('Samples')
+plt.ylabel('Amp.')
+plt.title(f'$CC_{{dry-est}}: {CCdry:.3f}$, $CC_{{drr-est}}: {CCdrr:.3f}$')
 
 
 # %%
+# np.corrcoef(whitening(Hdrr[-30:]), whitening(y_drr), '.')[0,1]
+# np.corrcoef(whitening(Hdry[-30:]), whitening(y_dry), '.')[0,1]
 
+
+
+# %%
